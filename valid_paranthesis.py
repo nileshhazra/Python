297117cc19a93ea22e218a23f78cda2eb9c49f44
Nilesh Hazra <nileshhz99@gmail.com>
2025-05-16 00:57:33 +0530

    add optimized code

--- valid_paranthesis.py
+++ valid_paranthesis.py
@@ -19,3 +19,32 @@ class Solution:
             return False
         else:
             return True
+
+    def isValid2(self, s):
+        stack = []
+        pairs = {"(": ")", "{": "}", "[": "]"}
+        for char in s:
+            if char in pairs:
+                stack.append(char)
+            else:
+                if len(stack) == 0:
+                    return False
+                last = stack.pop()
+                if pairs[last] != char:
+                    return False
+
+        if len(stack) > 0:
+            return False
+        else:
+            return True
+
+
+def main():
+    sol = Solution()
+    res1 = sol.isValid("[]({})")
+    res2 = sol.isValid2("[{(}]")
+    print(res1, res2)
+
+
+if __name__ == "__main__":
+    main()
